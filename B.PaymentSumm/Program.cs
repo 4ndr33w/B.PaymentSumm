fd956c571059444899c33d2a1c1c373a4716c503
4ndr33w <rx.081@hotmail.com>
2023-08-26 21:28:17 +0500

    Начал сначала. Консоль принимает поток данных; цены сортируются в списке _units

--- B.PaymentSumm/Program.cs
+++ B.PaymentSumm/Program.cs
@@ -8,79 +8,42 @@ namespace B.PaymentSumm
     {
         static void Main(string[] args)
         {
-            ushort countOfInputData = Convert.ToUInt16(Console.ReadLine());
+            ushort numberOfDatas = Convert.ToUInt16(Console.ReadLine());
 
-            string[,] array = new string[countOfInputData, 2];
+            //string[,] array = new string[numberOfDatas, 2];
 
-            for (int i = 0; i < countOfInputData; i++)
+            for (int i = 0; i < numberOfDatas; i++)
             {
-                string goodsCount = Console.ReadLine();
+                var goodsCount = Console.ReadLine();
+
                 var prices = Console.ReadLine().Split(' ');
-                //char[] chars = prices.ToCharArray().OrderBy(c => c.GetHashCode()).ToString().ToCharArray();
-                ushort[] units = new ushort[Convert.ToInt32(goodsCount)];
+                //ushort[] units = new ushort[Convert.ToUInt16(goodsCount)];
                 List<ushort> _units = new List<ushort>();
 
                 for (int j = 0; j < prices.Length; j++)
                 {
                     //units[j] = Convert.ToUInt16(prices[j]);
                     _units.Add(Convert.ToUInt16(prices[j]));
+
                 }
 
                 _units.Sort();
 
-                int sum = 0;
-                for (int k = 0; k < _units.Count; k++)
-                {
-                    int counter = 0;
-                    
-                    while (counter < 3) 
-                    {
-                        if (_units[k] == _units[k + 1])
-                        {
-                            counter++;
-                            sum += _units[k];
-                            k++;
-                        }
-                        else
-                        {
-                            k++;
-                            break;
-                        }
-                        
-                    }
-                    if (counter == 3)
-                    {
-                        sum -= _units[k];
-                        counter = 0;
-                    }
-                   
-                }
-                Console.WriteLine(sum);
-                //foreach (ushort unit in _units)
-                //{
-                //    Console.Write(sum);
-                //}
+                Console.WriteLine(Program.GetCountResult(_units, prices.Length));
 
-                //    array[i, 0] = goodsCount;
-                //    //array[i, 1] = chars.ToString();
-                //    prices = "";
-
-                //    for (int j = 0; j < chars.Length; j++)
-                //    {
-                //        prices += chars[j].ToString();
-                //    }
-
-                //    array[i, 1] = prices;
-                //}
-
-                //for (int i = 0; i < countOfInputData; i++)
-                //{
-                //    Console.WriteLine($"{array[i, 0]} / {array[i,1]}");
-                //}
 
                 Console.ReadKey();
                 //Console.WriteLine("Hello World!");
             }
+        }
+
+        private static string GetCountResult(List<ushort> sortedData, int goodsCount)
+        {
+            string result = "test";
+
+
+
+            return result;
         }
     }
 }
